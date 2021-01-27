080850ac7373b95a6e0c6e5abf72ab6dea9db01e
stellaymusu <stella7929@hotmail.com>
2021-01-28 00:50:04 +0800

    change ui

--- app/src/main/java/com/yumin/pomodoro/ui/main/adapter/ExpandableViewAdapter.java
+++ app/src/main/java/com/yumin/pomodoro/ui/main/adapter/ExpandableViewAdapter.java
@@ -11,11 +11,15 @@ import com.yumin.pomodoro.databinding.CategoryItemLayoutBinding;
 import com.yumin.pomodoro.databinding.MissionItemLayoutBinding;
 import com.yumin.pomodoro.utils.LogUtil;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class ExpandableViewAdapter extends ExpandableBaseAdapter<CategoryItemLayoutBinding, MissionItemLayoutBinding> {
     private static final String TAG = "[TestExpandableAdapter]";
     private OnClickListenerEditOrDelete onClickListenerEditOrDelete;
+    private static final int GROUP_COMING_POSITION = 1;
+    private Context mContext;
 
     public interface OnClickListenerEditOrDelete{
         void OnClickListenerDelete(Mission mission,int groupPosition,int childPosition);
@@ -27,6 +31,7 @@ public class ExpandableViewAdapter extends ExpandableBaseAdapter<CategoryItemLay
 
     public ExpandableViewAdapter(List<Category> dataList, Context context){
         super(dataList,context);
+        mContext = context;
     }
 
     public void flashCategory(List<Category> list) {
@@ -56,6 +61,7 @@ public class ExpandableViewAdapter extends ExpandableBaseAdapter<CategoryItemLay
 
     @Override
     public void onBindChildLayout(MissionItemLayoutBinding binding, Mission mission, int groupPosition, int childPosition, View view) {
+        LogUtil.logE(TAG,"[onBindChildLayout] groupPosition = "+groupPosition+" , childPosition = "+childPosition);
         binding.setMission(mission);
         binding.delete.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,10 +72,16 @@ public class ExpandableViewAdapter extends ExpandableBaseAdapter<CategoryItemLay
             }
         });
 
+        binding.itemTime.setText(mContext.getString(R.string.mission_time) +
+                Integer.toString(mission.getTime()) + mContext.getString(R.string.minute));
+
+        binding.itemGoal.setText(mContext.getString(R.string.mission_goal) +
+                Integer.toString(mission.getGoal()));
+
         // TODO: 1/18/21  gray out this item and show check icon when finished
         // set delete line if finished
         if (mission.isFinished()) {
-            binding.itemName.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
+//            binding.itemName.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG | Paint.ANTI_ALIAS_FLAG);
             view.setAlpha(0.5f); // set opacity
             binding.colorView.setVisibility(View.INVISIBLE);
             binding.finishedIcon.setVisibility(View.VISIBLE);
@@ -77,6 +89,16 @@ public class ExpandableViewAdapter extends ExpandableBaseAdapter<CategoryItemLay
             view.setAlpha(1f); // set opacity
             binding.colorView.setVisibility(View.VISIBLE);
             binding.finishedIcon.setVisibility(View.INVISIBLE);
+        }
+
+        if (groupPosition == GROUP_COMING_POSITION) {
+            binding.itemOperateDay.setVisibility(View.VISIBLE);
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd");
+            Date date = new Date(mission.getOperateDay());
+            String dateStr = simpleDateFormat.format(date);
+            binding.itemOperateDay.setText(mContext.getString(R.string.mission_operate_day)+dateStr);
+        }  else {
+            binding.itemOperateDay.setVisibility(View.GONE);
         }
     }
 }
